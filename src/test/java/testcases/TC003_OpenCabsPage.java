0ed2e4c3b4a257548ea9a085fc4af88f96a35a74
Tanmay <jtanmay246@gmail.com>
2026-02-25 11:49:48 +0530

    Screenshot and log added

--- src/test/java/testcases/TC003_OpenCabsPage.java
+++ src/test/java/testcases/TC003_OpenCabsPage.java
@@ -4,12 +4,14 @@ import basetest.BaseTest;
 import org.testng.Assert;
 import org.testng.annotations.Test;
 import pageObjects.CabBookingPage;
+import utilities.ScreenshotUtility;
 
 public class TC003_OpenCabsPage extends BaseTest {
     @Test
     public void openCabsPageTest() {
         CabBookingPage cabPage = new CabBookingPage(driver, wait);
         cabPage.openCabsPage();
+        ScreenshotUtility.takeScreenShot(driver, "Navigate_CabsPage");
         String actualTitle = driver.getTitle();
         String expectedTitle = "Cab Booking";
         Assert.assertTrue(actualTitle.contains(expectedTitle), "Cab booking page URL mismatch!");
--- src/test/java/testcases/TC006_PrintLowestPrice.java
+++ src/test/java/testcases/TC006_PrintLowestPrice.java
@@ -3,11 +3,13 @@ package testcases;
 import basetest.BaseTest;
 import org.testng.annotations.Test;
 import pageObjects.CabBookingPage;
+import utilities.ScreenshotUtility;
 
 public class TC006_PrintLowestPrice extends BaseTest{
     @Test
     public void printLowestPriceTest() {
         CabBookingPage cabPage = new CabBookingPage(driver, wait);
         cabPage.printLowestCabPrice();
+        ScreenshotUtility.takeScreenShot(driver, "CabsList_Results");
     }
 }
